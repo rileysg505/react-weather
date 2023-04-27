3c074f36913b998f2592713b76ece8d37affb670
Michelle Ly <lym8@uci.edu>
2023-04-27 16:07:26 -0700

    initial

---
+++ src/Api/getWeather.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+
+type Weather={
+    description: string;
+    icon: string
+}
+
+type Main={
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+}
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Irvine&appid=7fcec942249fb43b45edd62ab7ead6a9'
+
+const getWeather = async () => { 
+    await axios.get(url)
+    .then((response) => {
+        const weatherTemp = response.data.main
+        const weatherDescription = response.data.weather
+        const locationName = response.data.name
+        console.log(weatherTemp)
+        console.log(weatherDescription)
+        console.log(locationName)
+        return [weatherTemp, weatherDescription, locationName]
+    })
+    .catch((error: any) => {
+        console.log(error)
+    });
+    
+}
+
+// const getWeather = async () => {
+//     const weatherData = await axios.get(url)
+//     console.log(weatherData.data.name)
+//     return weatherData
+// }
+
+export default getWeather // purpose?
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from "react";
+import { ChangeEvent } from 'react';
+import getWeather from "./Api/getWeather";
+import "./App.css";
 
 function App() {
+  const [searchCityInput, setSearchCity] = useState('')
+  const [weather, setWeather] = useState(); // weather is current value of state, setWeather is function to update the state
+
+  useEffect(() => {
+    getWeather(); // returns weatherData
+  }, []); // tells useEffect to only render if certain values change
+
+  function changeCity(input: React.SetStateAction<undefined>) {
+    setWeather(input)
+  }
+  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newValue = e.currentTarget.value;
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather: {searchCityInput} </h1>
+      <h2>{getWeather}</h2>
+      <form id ='new-location-form'>
+        <input id='new-location-input'
+          type='text'
+          placeholder='Enter City Name'
+          value = {searchCityInput} 
+          onChange={(location) => setSearchCity(location.target.value)} />
+      </form>
+        <button id = "search-location-button" 
+          onClick={() => setSearchCity}> Search </button>
+      <ol className = 'list weather data'>
+          
+
+      </ol>
     </div>
+
   );
 }
+
 
 export default App;
